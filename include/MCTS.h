dbdc9cd2bcbb468c36e4cb7102993eeda0d8fdf0
CUPZYY <arne.habrekke@null.net>
2025-03-31 10:29:40 +0200

    Add function for making a random move

--- include/MCTS.h
+++ include/MCTS.h
@@ -1,11 +1,14 @@
 #ifndef MCTS_H
 #define MCTS_H
 #include <Game.h>
+#include <random>
 
 class MCTS {
    public:
     Game game;
     explicit MCTS(Game& game);
+   private:
+    Game randomMove();
 };
 
 #endif  // MCTS_H
--- src/solve/MCTS.cpp
+++ src/solve/MCTS.cpp
@@ -2,6 +2,17 @@
 
 #include <Game.h>
 
+#include <random>
+
 using namespace std;
 
-MCTS::MCTS(Game& game) : game(game) {}
+MCTS::MCTS(Game& game) : game(game) {}Game MCTS::randomMove() {
+    std::uniform_int_distribution<int> colDist(0, game.nonEmptyColumns.size() - 1);
+    const int col_i = game.nonEmptyColumns[colDist(rd)];
+
+    std::uniform_int_distribution<int> rowDist(0, game.grid[col_i].size() - 1);
+    const int row_i = rowDist(rd);
+
+    game.remove(col_i, row_i);
+    return game;
+}
